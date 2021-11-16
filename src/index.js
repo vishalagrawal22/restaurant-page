62f77407cb1d0df2b80c6d3885cdaa17eb612913
Vishal Agrawal <vishalagrawalva22@gmail.com>
2021-11-17 00:22:23 +0530

    add basic UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css";
+import foodImageUrl from "./images/food.jpg"
+
+const imageContainer = document.querySelector("#food-image-container");
+const foodImage = new Image();
+foodImage.src = foodImageUrl;
+foodImage.id = "food-image";
+imageContainer.appendChild(foodImage);
